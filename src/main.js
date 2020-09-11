5835c6069e2d4a297af7c97d6869a276e5558ea1
Alma <a.q.frankenstein@gmail.com>
2020-09-11 08:57:36 -0700

    add js to test button

--- src/main.js
+++ src/main.js
@@ -4,26 +4,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
+  $('#submitButton').click(function() {
+    const amount = parseInt($("inputNum").val());
+
+    $("#returnNum").text(amount);
+    $("#returnNum").show();
+  })
+});
+
+
+
     // const city = $('#location').val();
     // $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/USD`;
+//     let request = new XMLHttpRequest();
+//     const url = `https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/USD`;
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
+//     request.onreadystatechange = function() {
+//       if (this.readyState === 4 && this.status === 200) {
+//         const response = JSON.parse(this.responseText);
+//         getElements(response);
+//       }
+//     };
 
-    request.open("GET", url, true);
-    request.send();
+//     request.open("GET", url, true);
+//     request.send();
 
-    // function getElements(response) {
-      // $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
-  });
-});
+//     // function getElements(response) {
+//       // $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//       // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//     }
+//   });
+// });
